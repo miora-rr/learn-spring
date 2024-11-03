034a530973d5f94aec12019859ff8dc5ec968b57
miora-rr <miora.r.rakoto@gmail.com>
2024-11-03 18:55:36 -0500

    Create StudentDAO and saving on student in db

--- 02-jpa/cruddemo/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 02-jpa/cruddemo/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.luv2code.cruddemo;
 
+import com.luv2code.cruddemo.dao.StudentDAO;
+import com.luv2code.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,21 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello world");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		// Create student, save student object, display d of saved student
+		System.out.println("Creating new student object...");
+		Student tempStudent = new Student("Paul", "Doe", "paul.doe@email.com");
+
+		System.out.println("Saving student");
+		studentDAO.save(tempStudent);
+
+		//display student
+		System.out.println("Save student. Generated id: " + tempStudent.getId());
 	}
 }
