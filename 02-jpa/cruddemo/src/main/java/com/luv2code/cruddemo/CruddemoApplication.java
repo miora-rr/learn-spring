7697223ddabe89721b9e48bcea2f11ce2225a853
miora-rr <miora.r.rakoto@gmail.com>
2024-11-03 19:00:39 -0500

    Saving multiple student to db

--- 02-jpa/cruddemo/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 02-jpa/cruddemo/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -17,12 +17,26 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			createStudent(studentDAO);
+			//createStudent(studentDAO);
+
+			createMultipleStudent(studentDAO);
 		};
 	}
 
+	private void createMultipleStudent(StudentDAO studentDAO) {
+		System.out.println("Creating 3 students object...");
+		Student tempStudent1 = new Student("Joe", "Jonas", "joe.doe@email.com");
+		Student tempStudent2 = new Student("Nick", "Jonas", "nick.doe@email.com");
+		Student tempStudent3 = new Student("Kevin", "Jonas", "kevin.doe@email.com");
+
+		System.out.println("Saving out students");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+	}
+
 	private void createStudent(StudentDAO studentDAO) {
-		// Create student, save student object, display d of saved student
+		// Create student, save student object, display the saved student
 		System.out.println("Creating new student object...");
 		Student tempStudent = new Student("Paul", "Doe", "paul.doe@email.com");
 
