1debc65d6c5be22f027f130e5ac3341139e7d3a0
miora-rr <miora.r.rakoto@gmail.com>
2024-11-04 18:44:37 -0500

    Use path variable to find student by id

--- 03-rest-crud/demo/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ 03-rest-crud/demo/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.luv2code.demo.rest;
 
 import com.luv2code.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,16 +13,26 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
+
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadDate(){
+        students = new ArrayList<>();
 
         students.add(new Student("Nick", "Jonas"));
         students.add(new Student("Justin", "Bieber"));
         students.add(new Student("Joe", "Locke"));
-
+    }
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         // Behind the scene, use Jackson to map to JSON
         return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudentById(@PathVariable int studentId) {
+        return students.get(studentId);
     }
 }
 
