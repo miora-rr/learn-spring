126abbe71a6bdebea7db652c839b53e6e6a1e4a4
miora-rr <miora.r.rakoto@gmail.com>
2024-11-05 17:44:39 -0500

    Custom Rest path and number of pages - End of CRUD API section

--- 03-rest-crud/04-spring-boot-rest-employee-data-rest/src/main/java/com/luv2code/demo/dao/EmployeeRepository.java
+++ 03-rest-crud/04-spring-boot-rest-employee-data-rest/src/main/java/com/luv2code/demo/dao/EmployeeRepository.java
@@ -2,7 +2,10 @@ package com.luv2code.demo.dao;
 
 import com.luv2code.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// Custom path. Use members instead of employees
+// @RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
