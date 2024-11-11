a87c134032f4caf9048cebc0dc31e320f52383af
miora-rr <miora.r.rakoto@gmail.com>
2024-11-11 15:54:33 -0500

    Start Bidirectional. Important annotation is MappedBy in InstructorDetail class

---
+++ 05-advanced-jpa/02-jpa-one-to-one-bidirectional/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,43 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Instructor;
+import com.luv2code.cruddemo.entity.InstructorDetail;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AppDAOImpl implements AppDAO{
+
+    EntityManager entityManager;
+
+    @Autowired
+    public AppDAOImpl (EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+        // Since Cascade.all will also persist InstructorDetails
+        entityManager.persist(theInstructor);
+    }
+
+    @Override
+    public Instructor findInstructorById(int theId) {
+        return entityManager.find(Instructor.class, theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int id) {
+        Instructor tempInstructor = entityManager.find(Instructor.class, id);
+
+        entityManager.remove(tempInstructor);
+    }
+
+    @Override
+    public InstructorDetail findInstructorDetailById(int id) {
+        return entityManager.find(InstructorDetail.class, id);
+    }
+}
